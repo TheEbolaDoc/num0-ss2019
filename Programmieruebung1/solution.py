28ed1b1f5e8dae9bbf5a9b249601ae02f85fb387
Christian Heusel <christian@heusel.eu>
2019-05-20 19:01:03 +0200

    formatting
    Signed-off-by: Christian Heusel <christian@heusel.eu>

--- Programmieruebung1/solution.py
+++ Programmieruebung1/solution.py
@@ -15,7 +15,8 @@ class Neville:
         ]
 
     def compute(self, verbose=False):
-        max_len = 0
+        if verbose:
+            max_len = 0
         for k in range(len(self.pik) - 1):
             for i in range(k, len(self.pik[k]) - 1):
                 next_pik = self.pik[k][
@@ -23,9 +24,11 @@ class Neville:
                         self.grid_points[i+1][0] - self.grid_points[i-k][0]
                     ) * (self.pik[k][i+1] - self.pik[k][i])
                 if verbose:
-                    compute_str = f"{next_pik:5.4f} = {self.pik[k][i+1]:5.4f} + ({degrees(self.x):3.1f} - "
-                    compute_str += f"{degrees(self.grid_points[i+1][0]):3.1f})/({degrees(self.grid_points[i+1][0]):3.1f} - "
-                    compute_str += f"{degrees(self.grid_points[i-k][0]):4.1f}) * ({self.pik[k][i+1]:5.4f} - {self.pik[k][i]:5.4f}) | i={i}, k={k}"
+                    compute_str = f"{next_pik:5.4f} = {self.pik[k][i+1]:5.4f} + "
+                    compute_str += f"({degrees(self.x):3.1f} - {degrees(self.grid_points[i+1][0]):3.1f})"
+                    compute_str += f"/({degrees(self.grid_points[i+1][0]):3.1f} - {degrees(self.grid_points[i-k][0]):4.1f})"
+                    compute_str += f" * ({self.pik[k][i+1]:5.4f} - {self.pik[k][i]:5.4f})"
+                    compute_str += f" | i={i}, k={k}"
                     max_len = max(max_len, len(compute_str))
                     print(compute_str)
                 self.pik[k + 1].append(next_pik)
@@ -34,6 +37,7 @@ class Neville:
         return self.pik[len(self.pik)-1][len(self.pik[len(self.pik)-1]) - 1]
 
     def piktable(self):
+        """ this function only creates the table as we created """
         # transpose it
         local = [[self.pik[j][i] for j in range(len(self.pik))]
                  for i in range(len(self.pik[0]))]
@@ -69,11 +73,12 @@ def main():
     print("Result: ", sin_interpol.compute(verbose=True), "\n")
     print(sin_interpol.piktable())
 
+    # create a scaled version of the sin() function
     scaled_sin = lambda n: lambda x: sin(n*x)
-    for i in [2, 4, 8]:
-        px = [radians(45), scaled_sin(i), x_n]
-        sin_interpol_b = Neville(px)
-        print(sin_interpol_b.compute())
+    # for i in [2, 4, 8]:
+    #     px = [radians(45), scaled_sin(i), x_n]
+    #     sin_interpol_b = Neville(px)
+    #     print(sin_interpol_b.compute())
 
 if __name__ == "__main__":
     main()
